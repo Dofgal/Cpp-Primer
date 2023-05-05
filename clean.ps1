ed965fcd187e3c37d7a74dce4c7d377c13823d8f
Dofgal <61961958+Dofgal@users.noreply.github.com>
2023-05-05 20:38:57 +0800

    Minor changes

--- clean.ps1
+++ clean.ps1
@@ -1,2 +1,2 @@
-# Clear all the complied binary files ended with '.obj' and '.exe'
+# Clear all the compiled binary files ended with '.obj' and '.exe'
 Get-ChildItem -Path "$($PSSCriptRoot)\Chapter*" -Include @("*.obj", "*.exe") -Recurse | Remove-Item -Force
